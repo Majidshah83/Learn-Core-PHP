df4ca22483d305c60713f2bc2e2407b6549b3905
Majidshah83 <shahmajid508@gmail.com>
2022-03-20 15:54:07 +0500

    varibale

---
+++ varible.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+      $v1="majid";
+      $last="shah";
+      $result ="My name is:" .$v1 . ' ' . $last;
+      echo $result;
+
+    
+    ?>
+</body>
+
+</html>
